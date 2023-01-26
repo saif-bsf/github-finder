458937aaf7f6905a0f6d9b8fdc67b60e63fe17a4
Saif Sheikh <105776315+saif-bsf@users.noreply.github.com>
2023-01-26 15:15:43 +0400

    Add files via upload
    Initial Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details. This
+        project is build to experiment with github apis.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Layout By:
+        <a className="text-white" href="https://twitter.com/hassibmoddasser">
+          Saif Sheikh
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default About;
